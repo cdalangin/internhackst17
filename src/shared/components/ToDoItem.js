30642a8c1418432dded40db48dbc39fb28e28b33
cdalangin <florenzdalangin@gmail.com>
2021-07-21 13:54:58 -0400

    fix todo component

--- src/shared/components/ToDoItem.js
+++ src/shared/components/ToDoItem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -6,12 +6,14 @@ import { db } from '../../firebase/config';
 import { AuthContext } from '../../screens/auth/AuthContext';
 
 import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export default function ToDoListItem(props) {
     const { user } = useContext(AuthContext)
     const task = props.task
+    const [done, setDone] = useState(false)
 
     const remove = () => {
         const userRef = db.collection("users").doc(user.uid)
@@ -25,12 +27,19 @@ export default function ToDoListItem(props) {
         <View style={style.card}>
             <View style={style.container}>
                 {/* TODO: Use below button to delete todolist item */}
-                <View style={style.buttoncont}>
-                    <TouchableOpacity onPress={() => console.log("done!")}><MaterialIcons name="check-box-outline-blank" size={24} color="black" /></TouchableOpacity>
+                <View>
+                    <TouchableOpacity onPress={() => { setDone(!done) }}>
+                        {
+                            done ? <Ionicons name="checkbox" size={30} color="thistle" />
+                                :
+                                <Ionicons name="square-outline" size={30} color="thistle" />
+                        }
+
+                    </TouchableOpacity>
                 </View>
-                <View style={style.textcont}>
-                    <Text style={style.task}>{task}</Text>
-                </View>
+                {/* <View> */}
+                <Text style={style.task}>{task}</Text>
+                {/* </View> */}
 
             </View>
         </View>
@@ -41,11 +50,8 @@ export default function ToDoListItem(props) {
 
 const style = StyleSheet.create({
     card: {
-        // backgroundColor: "white",
-        // padding: 25,
-        // margin: 10,
         backgroundColor: "#fffbee",
-        padding: 25,
+        padding: 15,
         margin: 5,
         width: windowWidth - 30,
         borderRadius: 10,
@@ -54,16 +60,13 @@ const style = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
-    },
-    textcont: {
-        flexWrap: "wrap",
-        flexBasis: "auto"
+        maxWidth: windowWidth - 40,
     },
     task: {
         color: "#9A76A5",
-        fontSize: 18
-    },
-    buttoncont: {
-        flexBasis: 25
+        fontSize: 18,
+        textAlign: "right",
+        flex: 1,
+        flexWrap: 'wrap'
     },
 })
